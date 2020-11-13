1f4099407f399b8c5fc8ca2dc7eb3f2237d2d247
John Wesley <72558607+Jotinha65@users.noreply.github.com>
2020-11-13 08:59:46 -0300

    Fase 1
    Inicio do projeto

---
+++ prefeitura/src/test/java/br/com/zup/estrelas/prefeitura/PrefeituraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.prefeitura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefeituraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
